7825a04863fa7aa8bd32a0e8490fe5197a5eb648
mikicastro69 <castro-miquel@hotmail.com>
2021-03-02 14:40:06 +0100

    Añado todo el control del formulario controlando la creacion de palabras a la hora de hacer submit y el boton desabilitado

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,41 @@
-const
+const inputPalabra = document.querySelector(".input-palabra");
+const repeticiones = document.querySelector(".repeticiones");
+const submitNuevaPalabra = document.querySelector(".submit-palabra");
+const checkLenguajeProgramacion = document.querySelector(".lenguaje-programacion");
+const listaPalabras = document.querySelector(".lista-palabras");
+
+const updateValue = (e) => {
+  submitNuevaPalabra.disabled = e.target.value === "" || e.target.value.includes(" ");
+};
+
+const submitValue = (e) => {
+  e.preventDefault();
+  const createNode = crearPalabraValidador();
+  if (createNode) {
+    const nodoPalabra = document.createElement("li");
+    if (checkLenguajeProgramacion.checked) {
+      nodoPalabra.dataset.lenguaje = "si";
+    }
+    nodoPalabra.dataset.repeticiones = repeticiones.value;
+    nodoPalabra.textContent = inputPalabra.value;
+    console.log(nodoPalabra);
+    listaPalabras.appendChild(nodoPalabra);
+    submitNuevaPalabra.disabled = true;
+    inputPalabra.value = "";
+    repeticiones.value = "";
+  }
+};
+
+const crearPalabraValidador = () => {
+  let res = true;
+  const a = listaPalabras.children;
+  const b = [...a].map(paraula => paraula.innerHTML);
+  if (repeticiones.required && repeticiones.value === "") res = false;
+  if (inputPalabra.required && inputPalabra.value === "") res = false;
+  if (b.includes(inputPalabra.value)) res = false;
+
+  return res;
+};
+
+inputPalabra.addEventListener("input", updateValue);
+submitNuevaPalabra.addEventListener("click", submitValue);
